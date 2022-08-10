afad2b580d8c3c5497e3ab49f7ad9a85b5314761
waayyne <tohweijunwayne@gmail.com>
2022-08-09 20:02:38 -0700

    comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          ER
+          my name is wayne
         </p>
         <a
           className="App-link"
